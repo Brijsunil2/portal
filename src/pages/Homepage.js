f62771e2a1099a2ce1dfc51ddabe17c4da8b8ca8
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-07-01 14:55:52 -0400

    Implement layout for page body

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,7 +2,7 @@ import "./Homepage.css";
 
 const Homepage = () => {
   return (
-    <div className="container homepage-container w-50">
+    <div className="container homepage-container">
         <div className="apps-container background-container"></div>
         <div className="date-container background-container"></div>
         <div className="weather-container background-container"></div>
