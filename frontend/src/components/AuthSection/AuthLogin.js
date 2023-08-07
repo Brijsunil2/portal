5df437bb798e0d0c5cc746f26d40a84ced641527
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-08-07 18:39:46 -0400

    Completed Login and Signup layouts

--- frontend/src/components/AuthSection/AuthLogin.js
+++ frontend/src/components/AuthSection/AuthLogin.js
@@ -1,45 +1,73 @@
 import { useState } from "react";
 
 const AuthLogin = () => {
-  const [isUsernameInput, setIsUsernameInput] = useState(true);
-  const [userCredentials, setUserCredentials] = useState({username: "", password: ""});
+  const [isEmailInput, setIsEmailInput] = useState(true);
+  const userCredentials = {email: "", password: ""};
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const usernameInputOnChange = (e) => {
-    setUserCredentials({...userCredentials, username: e.target.value});
+  const showPasswordOnClick = () => {
+    const passwordInput = document.getElementById("loginPassInput");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+    } else {
+      passwordInput.type = "password";
+    }
   };
 
-  const onBtnClick = (e) => {
+  const onBtnClick = async(e) => {
     e.preventDefault();
-    setIsUsernameInput(false);
-    console.log(userCredentials)
+    setErrorMessage("");
+    if (isEmailInput) {
+      userCredentials.email = e.target.loginEmailInput.value;
+      setIsEmailInput(false);
+    } else {
+      userCredentials.password = e.target.loginPassInput.value;
+    }
+  };
+
+  const backBtnOnClick = () => {
+    setIsEmailInput(true);
+    userCredentials.email = "";
+    userCredentials.password = "";
   };
 
   return (
     <div className="authlogin-container">
       <h2 className="auth-h2">Login to get Started!</h2>
+      <p className="input-error-msg">{errorMessage}</p>
       <form className="d-flex flex-column" onSubmit={onBtnClick}>
         {
-        isUsernameInput ?
+        isEmailInput ?
           <input 
-            type="text" 
+            id="loginEmailInput"
+            type="email" 
             className="form-control" 
-            placeholder="Username"
-            minLength="6"
-            maxLength="25"
-            value={userCredentials.username}
-            onChange={usernameInputOnChange}
-            title=""
+            placeholder="Email"
             required />
           :
-          <input 
-            type="password" 
-            className="form-control" 
-            placeholder="Password"
-            value={userCredentials.password}
-            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-            required /> 
+          <>
+            <input 
+              id="loginPassInput"
+              type="password" 
+              className="form-control" 
+              placeholder="Password"
+              title="Password must have at least one lowercase, one uppercase, a number and at least 8 characters long."
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              required /> 
+            
+            <div className="form-check text-start py-2 align-self-end">
+              <input id="showPassCB" type="checkbox" className="form-check-input" onClick={showPasswordOnClick} />
+              <label className="form-check-label" htmlFor="showPassCB">Show Password</label>
+            </div>
+          </>
         }
-        <button type="submit" className="btn custom-btn align-self-end">{isUsernameInput ? "Next" : "Login"}</button>
+        <div>
+          {
+            !isEmailInput &&
+            <button type="button" className="btn custom-btn" onClick={backBtnOnClick}>Back</button>
+          }
+          <button type="submit" className="btn custom-btn float-end">{isEmailInput ? "Next" : "Login"}</button>
+        </div>
       </form>
     </div>
   )
--- frontend/src/components/AuthSection/AuthSignup.js
+++ frontend/src/components/AuthSection/AuthSignup.js
@@ -1,6 +1,90 @@
+import { useState } from "react";
 const AuthSignup = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const userCredentials = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: ""
+  };
+
+  const showPasswordOnClick = () => {
+    const passwordInput = document.getElementById("signupPassInput");
+    const repasswordInput = document.getElementById("signupRePassInput");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      repasswordInput.type = "text";
+    } else {
+      passwordInput.type = "password";
+      repasswordInput.type = "password";
+    }
+  };
+
+  const onBtnClick = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    userCredentials.firstname = e.target.signupFirstnameInput.value;
+    userCredentials.lastname = e.target.signupLastnameInput.value;
+    userCredentials.email = e.target.signupEmailInput.value;
+    userCredentials.password = e.target.signupPassInput.value;
+
+    if (e.target.signupRePassInput.value !== userCredentials.password) {
+      setErrorMessage("Passwords do not match");
+    } else {
+      
+    }
+  };
+
   return (
-    <div>AuthSignup</div>
+    <div className="authsignup-container">
+      <h2 className="auth-h2">Signup to get Started!</h2>
+      <p className="input-error-msg">{errorMessage}</p>
+      <form className="d-flex flex-column" onSubmit={onBtnClick}>
+        <div className="d-flex justify-content-between">
+          <input 
+            id="signupFirstnameInput"
+            type="text" 
+            className="form-control me-1" 
+            placeholder="Firstname"
+            required />
+          <input 
+            id="signupLastnameInput"
+            type="text" 
+            className="form-control ms-1" 
+            placeholder="Lastname"
+            required />
+        </div>
+        <input 
+          id="signupEmailInput"
+          type="email" 
+          className="form-control" 
+          placeholder="Email"
+          required />
+        <input 
+          id="signupPassInput"
+          type="password" 
+          className="form-control" 
+          placeholder="Password"
+          title="Password must have at least one lowercase, one uppercase, a number and at least 8 characters long."
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          required /> 
+        <input 
+          id="signupRePassInput"
+          type="password" 
+          className="form-control" 
+          placeholder="Retype Password"
+          title="Password must have at least one lowercase, one uppercase, a number and at least 8 characters long."
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          required /> 
+        <div className="form-check text-start py-2 align-self-end">
+          <input id="showPassCB" type="checkbox" className="form-check-input" onClick={showPasswordOnClick} />
+          <label className="form-check-label" htmlFor="showPassCB">Show Password</label>
+        </div>
+        <div>
+          <button type="submit" className="btn custom-btn float-end">Signup</button>
+        </div>
+      </form> 
+    </div>
   );
 };
 
